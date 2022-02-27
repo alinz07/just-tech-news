fa4af3af7bc7b390fc41aa37abb1ff8cfa3895de
Tony Linz <tonyllinz@gmail.com>
2022-02-26 19:59:44 -0600

    add vote model, put post-routes to include votes, create associations between models so vote is through table

--- models/Post.js
+++ models/Post.js
@@ -1,8 +1,32 @@
+const req = require('express/lib/request');
 const {Model, DataTypes} = require('sequelize');
 const sequelize = require('../config/connection');
 //const bcrypt = require('bcrypt');
 
-class Post extends Model {};
+class Post extends Model {
+    static upvote(body, models) {
+        return models.Vote.create({
+            user_id: body.user_id,
+            post_id: body.post_id
+        }).then (() => {
+            return Post.findOne({
+                where: {
+                    id: body.post_id
+                },
+                attributes: [
+                    'id',
+                    'post_url',
+                    'title',
+                    'created_at',
+                    [
+                        sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
+                        'vote_count'
+                    ]
+                ]
+            })
+        })
+    }
+};
 
 //define table columns and configuration
 Post.init(
@@ -59,7 +83,6 @@ Post.init(
         sequelize,
         //don't automatically create createdAt/updatedAt timestamps fileds
         timestamps: true,
-        updatedAt: false,
         //don't pluarlize name of database table
         freezeTableName: true,
         //use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
